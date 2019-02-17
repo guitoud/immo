d8f1c7a253177ff0fa97dfb735fee739661dac4c
Guillaume Dumas <guillaume.dumas-prestataire@labanquepostale.fr>
2019-02-17 18:59:07 +0100

    todo - 3

--- test.sh
+++ test.sh
@@ -5,10 +5,9 @@ set -x
 set -e
 $curl = /usr/bin/curl
 # la ton url d'acces
-export URL="http://example.com/lestreamjson/jsonrachidou.json"
+export URL="http://example.com/leStreamJson/jsonrachidou.json"
 # la ton directory ftp:
 export DIRNAME="/var/run/ftpd"
-
 
 function createTag()
         tags = $(cat jsonrachidou.json | jq -S '.posts[]|(.url|.tags[0].title)')
@@ -18,28 +17,37 @@ function createTag()
 		done
 	return 0
 
-function getTagComaFile()
+# exemple cd $dirDate ; createTag() va créer un répertoire tag dans le repertoire dirDate
+
+function getCSV($streamUrl)
+	# RAchidou bidou : quand tu doit remplacer le cat $streamurl par curl $streamURl avec ta requete es.
+	$tags, $url = $(cat $steamUrl | jq -r .posts[]| [(.tags[].title), .attachments[].images.full.url]|@csv)
+	return $tags, $url
+	# todo boucle for sur shift de la longueur tags quand len tags = 1 alors url
+
+$tags, $url = getCSV(jsonrachidou.json)
 	
-function getTag()
-	test = shift $@
-i	tag = $($curl $URL |  jq -S '.posts[]|{url: .url, tags: .tags[0].title} )' )
-	return $tag
+#function getTag()
+#	test = shift $@
+# 	tag = $($curl $URL |  jq -S '.posts[]|{url: .url, tags: .tags[0].title} )' )
+#	return $tag
+#
+#function getQuantieme()
+#        date = $(date +%j)
+#	return $date
+#
+#function getUrl($tag)
+#	fname = shift $@
+#	toto = curl -o $test $fname
+#	return $toto
+#
+#for I in liste_tag ; do
+#	tag = getTag(liste_tag)
+#	dateq = getQuantieme()
+# 	fname = getUrl(tag)
+#	cp fname $toto/$dateq
+#done
 
-function getQuantieme()
-        date = $(date +%j)
-	return $date
-
-function getUrl($tag)
-	fname = shift $@
-	toto = curl -o $test $fname
-	return $toto
-
-for I in liste_tag ; do
-	tag = getTag(liste_tag)
-	dateq = getQuantieme()
- 	fname = getUrl(tag)
-	cp fname $toto/$dateq
-done
 function creeLaDate()
 	racine=shift $@
 	dateJour=$(date +%j)
@@ -52,7 +60,7 @@ function downloadVideos()
 	listeUrl = getListeUrl($tag)
 	for I in $tag ; do
 		url = getUrl($tag)
-		test = `curl -O $ `
+		test = `curl -O $TODO`
 	done	
 
 downloadPath = creeLaDate($DIRNAME)
